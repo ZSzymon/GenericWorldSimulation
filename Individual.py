b7a34c08b0bfd0b569f9db4136f1318628162f56
Szymon Zywko <szymonzywko@gmail.com>
2022-01-22 20:39:00 +0100

    Add individual class

--- Individual.py
+++ Individual.py
@@ -5,10 +5,14 @@ from typing import List
 class Individual:
     chromosome: List[int]
     genesInChromosome: int
+    mutationChance: float
+    individualId: int
 
-    def __init__(self, genesInChromosome: int, geneMinVal: int, geneMaxVal: int):
+    def __init__(self, genesInChromosome: int, geneMinVal: int, geneMaxVal: int, mutationChance:float, individualId: int):
         self.genesInChromosome = genesInChromosome
         self.initializeChromosome(genesInChromosome, geneMinVal, geneMaxVal)
+        self.mutationChance = mutationChance
+        self.individualId = individualId
 
     def initializeChromosome(self, genesInChromosome: int, geneMinVal: int, geneMaxVal: int):
         for i in range(genesInChromosome):
